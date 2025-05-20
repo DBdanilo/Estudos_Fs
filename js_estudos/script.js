2679d8d2e126362486f0b90d8415278166b27423
Danilo Macedo <danilo.macedo4@gmail.com>
2025-05-20 14:17:13 -0300

    atualização do repositorio com arquivos da aula 01 de js

---
+++ js_estudos/script.js
@@ -0,0 +1,26 @@
+
+// Atividade 1_ aula_ 01
+
+/* var nome = "Danilo";
+var sobrenome = "Barbosa";
+var ultimoNome = "Maceo";
+
+var nomeCompleto = nome + " " + sobrenome + " " + ultimoNome;
+console.log("Nome completo:", nomeCompleto);
+alert("Nome completo: " + nomeCompleto);
+*/
+
+// Atividade 2_ aula_ 01
+
+var nome = "Danilo";
+var idade = 30;
+
+var nome = "Danilo";
+var idade = 25; 
+
+
+var mensagem = "Parabéns pelo seu aniversário, " + nome + "! 🎉 Hoje você celebra " + idade + " anos de histórias, conquistas e momentos incríveis! Que seu dia seja repleto de alegria e sucesso!";
+
+console.log(mensagem);
+alert(mensagem);
+
